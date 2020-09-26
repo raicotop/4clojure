51691b5121c91eed8b6166abf13a0b82f58cc4ff
Your Name <you@example.com>
2020-09-26 20:32:16 +0200

    Next 8 problems solved

--- solutions/src/solutions/core.clj
+++ solutions/src/solutions/core.clj
@@ -25,3 +25,24 @@
 (def sol-23 #(into '() %))
 (def sol-24 #(reduce + %))
 (def sol-25 #(filter odd? %))
+(def sol-26 (fn fib
+              [n]
+              (cond
+                (= n 1) [1]
+                (= n 2) [1 1]
+                :else (let [prev (fib (dec n))]
+                        (conj prev (reduce + (take 2 (reverse prev))))))))
+(def sol-27 #(= (seq %) (reverse %)))
+(def sol-28 (fn flatten
+              [s]
+              (if (coll? s)
+                (mapcat flatten s)
+                [s])))
+(def sol-29 #(apply str (re-seq #"[A-Z]" %)))
+(def sol-30 #(->> %
+                  (partition-by identity)
+                  (map first)))
+(def sol-31 #(partition-by identity %))
+(def sol-32 #(interleave % %))
+(def sol-33 #(mapcat (partial repeat %2) %1))
+(def sol-34 #())
